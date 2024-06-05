2a1fc274fb9bedc7a5d2ed381d0c67651be06a15
Hasibul Islam <hasibulislam@HasibsMdioM2Max.fritz.box>
2024-06-06 01:25:00 +0200

    Landing page hero has been completed

--- src/SharedPages/HeroPart.jsx
+++ src/SharedPages/HeroPart.jsx
@@ -1,11 +1,36 @@
 import { Link } from "react-router-dom";
+import UseAuth from "../Hooks/UseAuth";
 
 
 const HeroPart = () => {
+
+    const { user } = UseAuth();
+
     return (
         <div className="flex flex-row justify-around ">
           
 
+            <div className="card w-[290px] bg-base-100 shadow-xl">
+                <div className="card-body">
+                    <h2 className="card-title text-sm font-bold">Benefits of blood donation</h2>
+                    <p className="text-xs">If a dog chews shoes whose shoes does he choose?</p>
+                </div>
+                <figure><img className="w-52 mx-auto pb-5 rounded-xl " src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" /></figure>
+
+                {
+                    user ? <>
+                        <div className="mx-auto mb-3">
+                            <Link to='/payment'><button className="btn btn-accent btn-xs ">Donate Now</button></Link>
+                        </div>
+                    </>
+                        :
+                        <>
+                            <div className="mx-auto mb-3">
+                                <Link to='/signup'><button className="btn btn-accent btn-xs ">Become a Donor</button></Link>
+                            </div>
+                        </>
+                }
+            </div>
             <div className="card w-[290px] bg-base-100 shadow-xl">
                 <div className="card-body">
                     <h2 className="card-title text-sm font-bold">Benefits of blood donation</h2>
@@ -23,16 +48,14 @@ const HeroPart = () => {
                     <p className="text-xs">If a dog chews shoes whose shoes does he choose?</p>
                 </div>
                 <figure><img className="w-52 mx-auto pb-5 rounded-xl " src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" /></figure>
-            </div>
-            <div className="card w-[290px] bg-base-100 shadow-xl">
-                <div className="card-body">
-                    <h2 className="card-title text-sm font-bold">Benefits of blood donation</h2>
-                    <p className="text-xs">If a dog chews shoes whose shoes does he choose?</p>
+
+                <div className="mx-auto mb-3">
+                    <Link to='/signup'><button className="btn btn-accent btn-xs ">Become a Donor</button></Link>
                 </div>
-                <figure><img className="w-52 mx-auto pb-5 rounded-xl " src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" /></figure>
             </div>
 
            
+            
         </div>
     );
 };
