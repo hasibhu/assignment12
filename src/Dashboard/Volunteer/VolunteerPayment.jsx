a4012f29bead8bd41d3a8c37e4ad4196c9d7e933
Hasibul Islam <hasibulislam@HasibsMdioM2Max.fritz.box>
2024-06-10 21:41:20 +0200

    donorpaymane and volunteerpayement are  done

---
+++ src/Dashboard/Volunteer/VolunteerPayment.jsx
@@ -0,0 +1,29 @@
+
+
+
+
+// import SectionTitle from '../SharedPages/SectionTitle';
+import { loadStripe } from "@stripe/stripe-js";
+import { Elements } from "@stripe/react-stripe-js";
+import CheckOutForm from "../Payment/CheckOutForm";
+
+
+
+
+// TODO: add publishable key
+const stripePromise = loadStripe(import.meta.env.VITE_PAYMENT_GATEWAY_PK);
+
+const VolunteerPayment = () => {
+    return (
+        <div className='bg-slate-300 m-28 pt-1 pb-10'>
+            {/* <SectionTitle heading="Payment" subHeading="Please Now"></SectionTitle> */}
+            <div>
+                <Elements stripe={stripePromise}>
+                    <CheckOutForm></CheckOutForm>
+                </Elements>
+            </div>
+        </div>
+    );
+};
+
+export default VolunteerPayment;
